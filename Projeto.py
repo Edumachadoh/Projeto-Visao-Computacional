af3c6cf58792f7537fed57ea00e5746376f8265b
Edumachadoh <dudu.henrique.em@gmail.com>
2024-10-30 21:51:16 -0300

    Adicionando pincel

--- Projeto.py
+++ Projeto.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 #inicialiar o modelo de deteccao de mãos do mediapipe
 mp_maos = mp.solutions.hands #módulo com modelo pré treinado 
@@ -8,8 +9,23 @@ maos = mp_maos.Hands()
 mp_desenho = mp.solutions.drawing_utils
 
 
+
 #cap recebe a captura de vídeo (o numero serve para selecionar qual das cameras será usada)
-cap = cv2.VideoCapture(2);
+cap = cv2.VideoCapture(0)
+
+#variavel para armazenar a posição anterior do dedo indicador
+posicao_anterior = None
+
+#determina altura e largura do quadro que vai ser desenhado
+largura, altura = 640, 480
+
+#determinar tamanho da captura da camera com base na altura e largura pre determinadas
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, largura)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, altura)
+
+#mp.zeros cria uma matriz com pontos de valor RGB[0,0,0], que supostamente ficam atras do video da camera
+#quando for pintado algo na tela esse ponto na matriz muda de valor, logo tem cor e aparece na camera
+tela = np.zeros((altura, largura, 3), dtype=np.uint8)
 
 
 while True:
@@ -18,6 +34,8 @@ while True:
     #se ret der falso, significa que tem problema, e para o loop
     if not ret: 
         break
+
+    frame = cv2.flip(frame, 1)
 
     #converte BGR para RGB (BGR é o padrão do OpenCV, e RGB o que o MediaPipe espera)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -29,11 +47,24 @@ while True:
     if resultado.multi_hand_landmarks:
         #para cada ponto na mão, roda uma vez o loop
         for marcacao_mao in resultado.multi_hand_landmarks:
+            #obter coordenadas x e y do indicador
+            cx = int(marcacao_mao.landmark[8].x * largura)
+            cy = int(marcacao_mao.landmark[8].y * altura)
+            
+            #verifica se posicao anterior é nula, caso nao for ele desenha uma linha com base na posicao anterior
+            #tela=onde esta matriz a ser desenhada, (cx, cy)=posicao do dedo indicador an tela, (255, 255, 255)= cor do desenho,5=espessura
+            if posicao_anterior:
+                cv2.line(tela, posicao_anterior, (cx, cy), (255, 255, 255), 5)
+            posicao_anterior = (cx, cy)
+            
             #desenha os pontos de conexão no frame do vídeo (como referencia tem o frame, a mão e os pontos da mão)
             mp_desenho.draw_landmarks(frame, marcacao_mao, mp_maos.HAND_CONNECTIONS)
 
-    #chama a função imshow, em aspas é o nome que ta em cima do programa, e frame o frame atual q ta no loop
-    cv2.imshow("Video12312", frame)
+    #combinar tela da camera com tela que foi feito o desenho
+    frame_com_desenho = cv2.add(frame, tela)
+    
+    #chama a função imshow, em aspas é o nome que ta em cima do programa, e frame o frame atual q ta no loop, porem agora é desenho + camera
+    cv2.imshow("Video12312", frame_com_desenho)
 
     #verifica se a tecla "q" foi apertada, se sim ele sai do programa
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -42,4 +73,4 @@ while True:
 #desativa o uso da câmera
 cap.release()
 #fecha todas as telas abertas pelo cv
-cv2.destryAllWindows()
+cv2.destroyAllWindows()
